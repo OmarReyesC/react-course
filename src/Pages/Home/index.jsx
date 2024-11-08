b413a5b9e8a6bfab92bb4f7804af54f405b03510
OmarReyesC <omareyescar@gmail.com>
2024-11-08 13:26:06 -0600

    Class 8 API, completed

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,26 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(undefined);
 
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
     return (
     
       <Layout>
         Home
-        <Card></Card>
+        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+          {
+            items?.map((item) => (  //<= Estos paréntesis pueden también sustituir a la palabra reservada return
+              <Card key={item.id} data={item} />
+            ))
+          }
+        </div>
       </Layout>
     
     );
