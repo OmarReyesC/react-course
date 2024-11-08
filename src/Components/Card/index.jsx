1d9a918f9a75c8a476e13560af80ee8d5aac6387
OmarReyesC <omareyescar@gmail.com>
2024-11-08 16:13:55 -0600

    Class 10 completed

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,10 +1,18 @@
-const Card = ({ data }) => {  //Deestructurado para no tener que hacer data.data en todo.
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+
+const Card = ({ data }) => {  //Deestructurado para no tener que hacer data.data. en toda la info de la API.
+    const context = useContext(ShoppingCartContext);
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
             <figure className="relative mb-4 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.category}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.image} alt={data.title} />
-                <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">+</div>
+                <div 
+                className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1" 
+                onClick={() => context.setCount(context.count + 1)} >
+                    +
+                </div>
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light">{data.title}</span>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,10 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const ShoppingCartContext = createContext();
+export const ShoppingCartContext = createContext();
 
 export const ShoppingCartContextProvider = ({children}) => {
+    const [count, setCount] = useState(0);
+    console.log('Count:', count);
     return (
-        <ShoppingCartContext.Provider>
+        <ShoppingCartContext.Provider value={{
+            count,
+            setCount
+        }}>
             {children}
         </ShoppingCartContext.Provider>
     )
