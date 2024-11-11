2f9ba61f97785c4c2bba4bfe51ad2e0dfd568f4f
OmarReyesC <omareyescar@gmail.com>
2024-11-11 10:28:52 -0600

    Class 12 Icons, completed

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -11,7 +11,9 @@ const Card = ({ data }) => {  //Deestructurado para no tener que hacer data.data
                 <div 
                 className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1" 
                 onClick={() => context.setCount(context.count + 1)} >
-                    +
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
                 </div>
             </figure>
             <p className="flex justify-between">
