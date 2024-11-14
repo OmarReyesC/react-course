f29b3953181a5f9ac6f66043c69ed57dac3bc854
OmarReyesC <omareyescar@gmail.com>
2024-11-14 14:07:47 -0600

    Class 25 completed

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -2,12 +2,19 @@ const OrdersCard = props => {
     const { totalPrice, totalProducts } = props;
 
     return (
-        <div className="flex justify-between items-center mb-3 border border-black">
-            <p>
-                <span>01.02.23</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
-            </p>
+        <div className="flex justify-between items-center mb-4 border border-black p-4 w-80 rounded-lg">
+            <div className="flex justify-between w-full">
+                <p className="flex flex-col">
+                    <span className="font-light">01.02.23</span>
+                    <span className="font-light">{totalProducts} items</span>
+                </p>
+                <p className="flex items-center gap-2">
+                    <span className="font-medium text-2xl">{totalPrice}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" className="h-6 w-6 text-black cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>
+                </p>
+            </div>
         </div>
     );
 };
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -9,8 +9,8 @@ function MyOrders() {
   return (
     
       <Layout>
-        <div className="flex items-center justify-center relative w-80">
-          <h1>My Orders</h1>
+        <div className="flex items-center justify-center relative w-80 mb-4">
+          <h1 className="font-medium text-2xl">My Orders</h1>
         </div>
         {
           context.order.map((order, index) => (  // < Paréntesis para que se entienda 'return'
